b48df3ee250fa461a3fe2ae573e44f04090a76ba
BB <bartosz.swiadkowski@gmail.com>
2024-10-29 14:27:36 +0100

    Added logging

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 #source: https://codewithsusan.com/
 import requests
 from bs4 import BeautifulSoup
+import logging
+import logging.handlers
+
+# Logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
 
 # Specify the URL
 url = "https://www.accuweather.com/en/pl/wroc%C5%82aw/273125/weather-forecast/273125?city=wroc%C5%82aw"
@@ -26,7 +41,10 @@ if response.status_code == 200:
         # Extract and print the contents of the div
         temperature = temp_div.get_text(strip=True)
         print(f"Temperature in Wrocław: {temperature} ")
+        logger.info(f'Temperature in Wrocław: {temperature}')
     else:
         print("Div with class 'temp' not found on the page.")
+        logger.info(f"Failed to retrieve div with class 'temp'.")
 else:
     print(f"Failed to retrieve the page. Status code: {response.status_code}")
+    logger.info(f"Failed to retrieve the page. Status code: {response.status_code}")
